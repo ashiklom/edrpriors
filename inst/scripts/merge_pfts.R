40f62ac7c7e0266169e5f9245ba5c45635955b68
Alexey Shiklomanov <ashiklom@bu.edu>
2017-01-23 13:33:19 -0500

    Add PFT merging script

---
+++ inst/scripts/merge_pfts.R
@@ -0,0 +1,43 @@
+library(dtplyr)
+library(dplyr)
+library(data.table)
+
+bety <- src_postgres(dbname = 'bety',
+                     user = 'bety',
+                     password = 'bety')
+
+dat <- readRDS('../extdata/prospect_results.rds')
+
+my_species <- distinct(dat, SpeciesCode)
+
+ed_modeltype <- 1
+
+used_pfts <- c('temperate.Early_Hardwood',
+               'temperate.Mid_Hardwood',
+               'temperate.Late_Hardwood',
+               'temperate.North_Pine',
+               'temperate.Late_Conifer')
+
+#tbl(bety, 'pfts') %>%
+    #filter(modeltype_id == ed_modeltype) %>%
+    #select(pft.id = id, pft.name = name) %>%
+    #filter(pft.name %like% 'Optics%')
+
+ed_pft_species <- tbl(bety, 'species') %>%
+    select(species.id = id, SpeciesCode = AcceptedSymbol) %>%
+    inner_join(tbl(bety, 'pfts_species') %>%
+               select(species.id = specie_id, pft.id = pft_id)) %>%
+    inner_join(tbl(bety, 'pfts') %>%
+               filter(modeltype_id == ed_modeltype,
+                      name %in% used_pfts) %>%
+               select(pft.id = id, pft.name = name)) %>%
+    filter(SpeciesCode != '') %>%
+    select(SpeciesCode, pft.name) %>%
+    collect(n = Inf) %>%
+    setDT() %>%
+    setkey(SpeciesCode)
+
+merged_species <- inner_join(ed_pft_species, my_species)
+
+dat_pft <- inner_join(dat, merged_species)
+saveRDS(dat_pft, '../extdata/dat_pft.rds')
